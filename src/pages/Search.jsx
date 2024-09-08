b65a391c6b4150784d23cf6297d3accdf669f4d5
hongsun2da <hongsun2da@gmail.com>
2024-09-08 13:10:55 +0900

    process.env 환경변수 접근 다시 확인

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -8,6 +8,8 @@ const Search = () => {
     const [videos, setVideos] = useState([]);
 
     useEffect(() => {
+        console.log(process.env.REACT_APP_YOUTUBE_API_KEY); // API KEY 출력
+
         fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=48&q=${searchId}&type=video&key=AIzaSyBRc4yYwhpN-d8xgfMerKH6wt94blibNQM`,)
         .then(response => response.json())
         .then(result => {
