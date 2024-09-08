b2eeb81ba5871ef17ddcda9018819d90b068f1ca
HongSun2da <76771747+HongSun2da@users.noreply.github.com>
2024-09-08 16:54:37 +0900

    develop <- Feature/rapidapi_240908 머지 요청 (#9)
    * Rapid Api 로 변경 작업 - api 두번 호출 현상 발생 수정해야 함
    
    * 비디오 상세 페이지 만들기
    
    * 채널 데이터 불러오기
    
    * scrollTo 작업하기
    
    * 기타 버그 수정 하기

--- src/pages/Channel.jsx
+++ src/pages/Channel.jsx
@@ -1,12 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Main from '../components/section/Main'
+import { useParams } from 'react-router-dom'
+import { fetchFromAPI } from '../utils/api'
+
+import { CiBadgeDollar } from "react-icons/ci";
+import { CiMedal } from "react-icons/ci";
+import { CiRead } from "react-icons/ci";
+
 
 const Channel = () => {
+    const { channelId } = useParams();
+    const [ channelDetail, setChannelDetail ] = useState();
+
+    useEffect(() => {
+        console.log(channelId);
+        const fetchResults = async () => {
+            const data = await fetchFromAPI(`channels?part=snippet&id=${channelId}`);
+            setChannelDetail(data.items[0]);
+        };
+        fetchResults();
+    }, []);
+
     return (
         <Main 
             title = "유튜브 채널"
-            description="유튜브 채널페이지입니다.">
-            Channel
+            description="유튜브 채널페이지입니다."
+        >
+            {channelDetail &&
+                <section id='channel'>
+                <div className='channel__header' style={{ backgroundImage: `url(${channelDetail.brandingSettings.image.bannerExternalUrl})` }}>
+                    <div className='circle'>
+                        <img src={channelDetail.snippet.thumbnails.high.url} alt={channelDetail.snippet.title} />
+                    </div>
+                </div>
+                <div className='channel__info'>
+                    <h3 className='title'>{channelDetail.snippet.title}</h3>
+                    <p className='desc'>{channelDetail.snippet.description}</p>
+                    <div className='info'>
+                        <span><CiBadgeDollar />{channelDetail.statistics.subscriberCount}</span>
+                        <span><CiMedal />{channelDetail.statistics.videoCount}</span>
+                        <span><CiRead />{channelDetail.statistics.viewCount}</span>
+                    </div>
+                </div>
+            </section>
+            }
         </Main>
     )
 }
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,31 +1,58 @@
 import React, { useEffect, useState } from 'react'
-import Main from '../components/section/Main'
 import { useParams } from 'react-router-dom'
-import VideoSearch from '../components/videos/VideoSearch';
+import Main from '../components/section/Main'
+
+
+import { fetchFromAPI } from '../utils/api'
+import VideoSearch from '../components/videos/VideoSearch'
 
 const Search = () => {
-    const {searchId} = useParams();
-    const [videos, setVideos] = useState([]);
+    const { searchId } = useParams();
+    const [ videos, setVideos ] = useState([]);
+    const [ nextPageToken, setNextPageToken] = useState(null);
+    const [ loading, setLoading ] = useState(true);
 
     useEffect(() => {
-        console.log(process.env.REACT_APP_YOUTUBE_API_KEY); // API KEY 출력
-
-        fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=48&q=${searchId}&type=video&key=AIzaSyBRc4yYwhpN-d8xgfMerKH6wt94blibNQM`,)
-        .then(response => response.json())
-        .then(result => {
-            setVideos(result.items)
-        })
-        .catch(error => console.log(error));
+        //setVideos([]);
+        fetchVidoes(searchId);
+        setLoading(true);
     }, [searchId]);
+
+    const fetchVidoes = (query, pageToken = '') => {
+        fetchFromAPI(`search?part=snippet&type=video&q=${query}&pageToken=${pageToken}`)
+            .then((data) => {
+                setNextPageToken(data.nextPageToken);
+                setVideos((preVideos) => [...preVideos, ...data.items]);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.log('Error fetching data', error);
+                setLoading(false);
+            })
+    }
+
+    const handleLoadMore = () => {
+        if(nextPageToken){
+            fetchVidoes(searchId, nextPageToken);
+        }
+    }
+
+    const searchPageClass = loading ? 'isLoading' : 'isLoaded';
 
     return (
         <Main 
             title = "유투브 검색"
-            description="유튜브 검색 결과 페이지입니다."
-        >
-            <section id='searchPage'>
+            description="유튜브 검색 결과 페이지입니다.">
+            
+            <section id='searchPage' className={searchPageClass}>
+                <h2>🤠 <em>{searchId}</em> 검색 결과입니다.</h2>
                 <div className="video__inner search">
                     <VideoSearch videos={videos} />
+                </div>
+                <div className='video__more'>
+                    {nextPageToken && (
+                        <button onClick={handleLoadMore}>더 보기</button>
+                    )}
                 </div>
             </section>
         </Main>
