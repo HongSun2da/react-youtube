f20f6aa094289b804e2f79878e85240e8a1e4eaf
hongsun2da <hongsun2da@gmail.com>
2024-09-08 10:40:12 +0900

    메인 추천 영상 로딩 및 데이터 작업

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import Developer from '../components/contents/Developer'
 import VideoSlider from '../components/videos/VideoSlider'
 
 import { developerText } from '../data/developer';
+import { todayText } from '../data/today'
 import { webdText } from '../data/webd'
 import { websiteText } from '../data/website'
 import { gsapText } from '../data/gsap'
@@ -14,7 +15,7 @@ import { youtubeText } from '../data/youtube'
 const Home = () => {
     return (
         <Main title='웹스토리보이 유튜브' description='웹스토리보이 유튜버 사이트에 오신 것을 환영합니다.'>
-            <Today />
+            <Today videos={todayText} id="today"/>
             <Developer videos={developerText} title="😪 추천 개발자를 소개합니다." id="developer" />
             <VideoSlider videos={webdText} title="😮 웹디자인기능사 준비는 이걸로!" id="webd" />
             <VideoSlider videos={websiteText} title="😛 웹표준 사이트 만들기 기초 다지기" id="website" />
