e09f171a60b93f0b13f2d801cf93e630573ce1c4
HongSun2da <76771747+HongSun2da@users.noreply.github.com>
2024-09-08 13:29:40 +0900

    develop <- Feature/page 머지 요청 (#7)

--- src/pages/Developer.jsx
+++ src/pages/Developer.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Main from '../components/section/Main'
 
 import { developerText } from '../data/developer'
 import { Link } from 'react-router-dom'
 
 const Developer = () => {
+    const [loading, setLoading] = useState(true); 
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 300);
+    }, []);
+
+    const developerPageClass = loading ? 'isLoading' : 'isLoaded';
+
     return (
         <Main 
             title = "추천 개발자"
-            description="오늘의 추천 개발자 유튜버입니다."
-        >
-            <section id='developerPage'>
+            description="오늘의 추천 개발자 유튜버입니다.">
+            
+            <section id='developerPage' className={developerPageClass}>
                 <h2>🥰 오늘의 추천 개발자입니다.</h2>
                 <div className="developer__inner">
                     {developerText.map((developer, key) => (
@@ -22,7 +32,7 @@ const Developer = () => {
                             </div>
                             <div className="developer__info">
                                 <Link to={`/channel/${developer.channelId}`}>
-                                     {developer.name}
+                                    {developer.name}
                                 </Link>
                             </div>
                         </div>
