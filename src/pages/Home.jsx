66b9ba148c2d726ffde47e481167796bd46f9a1a
hongsun2da <hongsun2da@gmail.com>
2024-09-08 10:29:25 +0900

    메인 추천 개발자 로딩 효과 넣기

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Today from '../components/contents/Today'
 import Developer from '../components/contents/Developer'
 import VideoSlider from '../components/videos/VideoSlider'
 
+import { developerText } from '../data/developer';
 import { webdText } from '../data/webd'
 import { websiteText } from '../data/website'
 import { gsapText } from '../data/gsap'
@@ -14,7 +15,7 @@ const Home = () => {
     return (
         <Main title='웹스토리보이 유튜브' description='웹스토리보이 유튜버 사이트에 오신 것을 환영합니다.'>
             <Today />
-            <Developer />
+            <Developer videos={developerText} title="😪 추천 개발자를 소개합니다." id="developer" />
             <VideoSlider videos={webdText} title="😮 웹디자인기능사 준비는 이걸로!" id="webd" />
             <VideoSlider videos={websiteText} title="😛 웹표준 사이트 만들기 기초 다지기" id="website" />
             <VideoSlider videos={gsapText} title="🤓 GSAP 패럴랙스 효과를 하고 싶다면!" id="gsap" />
