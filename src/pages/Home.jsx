d217a6bd27536205e1ceb96ea9dbbac6b5f4d26b
hongsun2da <hongsun2da@gmail.com>
2024-09-08 10:42:26 +0900

    매인 화면 TEST 완료 확인 - 머지 요청함

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,6 +12,7 @@ import { gsapText } from '../data/gsap'
 import { portfolioText } from '../data/portfolio'
 import { youtubeText } from '../data/youtube'
 
+
 const Home = () => {
     return (
         <Main title='웹스토리보이 유튜브' description='웹스토리보이 유튜버 사이트에 오신 것을 환영합니다.'>
