4d92ea8ae3ef5bf292165dd6606dd51c3487d6a2
HongSun2da <76771747+HongSun2da@users.noreply.github.com>
2024-09-08 13:27:44 +0900

    develop <- Feature/search 머지 요청 (#6)
    * 검색 영역 설정하기
    
    * 유튜브 API 데이터 가져오기 확인
    
    * VideoSearch 컴퍼넌트 작업
    
    * process.env 환경변수 접근 다시 확인
    
    * Search 기능 TEST 확인

--- src/components/section/Search.jsx
+++ src/components/section/Search.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Search = () => {
+    const [searchKeyword, setSearchKeyword] = useState('');
+    const navigate = useNavigate();
+
+    const handleSearch = () => {
+        // console.log(searchKeyword);
+        if(searchKeyword){
+            navigate(`/search/${searchKeyword}`);
+            setSearchKeyword('');
+        }
+    };
+
     return (
         <div id='search'>
             <div className='search__inner'>
@@ -13,6 +25,12 @@ const Search = () => {
                     name='searchInput'
                     id='searchInput'
                     autoComplete='off'
+                    onChange={(e)=> (setSearchKeyword(e.target.value))}
+                    onKeyDown={e => {
+                        if(e.key === 'Enter'){
+                            handleSearch();
+                        }
+                    }}
                 />
             </div>
         </div>
